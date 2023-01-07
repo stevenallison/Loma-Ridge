b220290a25b15d198a0827438597ddf2f983c0ac
Steven Allison <allisons@dhcp-61-207.bio.uci.edu>
2023-01-06 17:06:56 -0800

    Adding precipitation scripts

---
+++ LomaPrecipitationCalculations.R
@@ -0,0 +1,124 @@
+################################################################################
+####### This code uses all clean files to make one master file        ##########                          
+####### Every new year that precipitation is collected should         ##########
+####### be added to the same folder in the Microbes and Global Change ##########
+####### drive.                                                        ##########
+################################################################################
+
+## Call your downloaded packages
+library(tidyr)
+library(purrr)
+library(readr)
+library(dplyr)
+
+################################################################################
+## Give you access to the folder in Microbes and Global Change
+## that contain all standardize clean files, called "RawData" . 
+## In this folder each "CleanRaw" file corresponds to a year.
+## This folder also has the watering file and the drought dates file.
+################################################################################
+
+################################################################################
+## Call all your clean raw files to be part of hourly.files to create an      ##
+## object to perform analyses.                                                ##
+## All columns for each file need to have the same name and must be on the    ##
+## same order for list.files to work properly.                                ##
+## NOTE: in the function "path" we call your local directory where            ##
+## all csv_files were stored.                                                 ##
+################################################################################
+
+# Create a list with all "CleanRaw" files which are use to create a dataframe
+# Every single files that you want to determine daily precipitation needs to
+# have "CleanRaw" to be added to this data frame
+Files <- drive_get("RawData", shared_drive = "Microbes and Global Change") %>%
+  drive_ls(pattern = "CleanRaw")
+
+  walk(drive_read_string(Files$id[1],encoding="UTF-8"))
+
+
+AllPrecip <- list.files(path = "RawData", pattern = "CleanRaw",ignore.case = TRUE, full.names = T) %>%
+  map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
+  filter(Precipitation>0) %>% # keep non-zero measurements
+  mutate(Day = as.Date(Day,format="%m/%d/%Y")) %>%
+  arrange(Day)
+
+AllPrecip <- list.files(path = "RawData", pattern = "CleanRaw",ignore.case = TRUE, full.names = T) %>%
+  map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
+  filter(Precipitation>0) %>% # keep non-zero measurements
+  mutate(Day = as.Date(Day,format="%m/%d/%Y")) %>%
+  arrange(Day)
+
+# Some daily values were calculated using data from more than one source
+# We will create a extra column to retain that information to show sources.
+# We will create "Source1" and "Source2". 
+# NA in Source2 means that there was not a second source of precipitation 
+# for that day. The WARNING it is for those rows that did not have
+# a value to add in Source2.
+GaugeSource <- AllPrecip %>%
+  group_by(Day) %>% # group rows by days
+  summarize(Source = paste(unique(sort(Source)), collapse = " ")) %>% # Collects all unique sources
+  separate(Source, c("Source1", "Source2"), " ", extra = "merge" ) # creates a column per source
+
+DailyPrecip <- AllPrecip %>%
+  group_by(Day) %>% # group rows by days
+  summarize(Ambient = sum(as.numeric(Precipitation))) %>% # Calculate daily sums
+  inner_join(GaugeSource,by="Day") # join source.name with precipitation by day
+
+################################################################################
+#### Here we add the dates that plots were closed                             ##
+################################################################################
+
+# Use the actual name of the file since there is only one. 
+# Make sure the updated file is the name in the code below
+# Create consecutive dates for the range of time that a plot was closed
+# We also need to create a column called Reduced to add 0 as precipitation.
+# Join Closures object with DailyPrecip object
+# Note: This object will have new dates that correspond to Closures
+AmbientReduced <- read.csv("RawData/ShelterClosureDates.csv") %>%
+  select(c(ClosureStart,ClosureEnd)) %>%
+  mutate(ClosureStart = as.Date(ClosureStart,format="%m/%d/%Y")) %>% #format day
+  mutate(ClosureEnd = as.Date(ClosureEnd,format="%m/%d/%Y"))%>% #format day
+  mutate(Day = as.Date(ClosureStart,format="%Y-%m-%d")) %>% #same date as closureStart to join
+  mutate(Day = purrr::map2(ClosureStart, ClosureEnd, seq, "day")) %>% #create sequence
+  unnest(Day, keep_empty = TRUE) %>%
+  mutate(Reduced = as.numeric(0)) %>% #Create a new numeric column
+  select(Day, Reduced) %>%
+  full_join(DailyPrecip, by="Day") %>%
+  arrange(Day) %>%
+  replace_na(list(Ambient = 0)) %>% # Add Zeros for those dates that were added by ExcludedPrecip object
+  mutate(Reduced = if_else(is.na(Reduced), Ambient, Reduced)) # Copy precipitation from AmbientPrecip to Drought 
+
+################################################################################
+#### Here we add the dates added water information                            ##
+################################################################################
+
+# Use the actual name of the file since there is only one. 
+# Make sure the updated file is the name in the code below
+# Join with ReducedAmbient object
+# This will create dates that were not included in ReducedAmbient
+
+FullPrecipLoma <-read.csv("RawData/WaterAdditionDates.csv") %>%
+  mutate(Date_Added = as.Date(Date_Added,format="%m/%d/%Y")) %>%
+  rename(Day = Date_Added) %>%
+  select(Day, Water_Added) %>%
+  full_join(AmbientReduced, by="Day") %>%
+  replace_na(list(Ambient = 0, Reduced = 0, Water_Added = 0)) %>% # Replace missing values with zero
+  mutate(Added = Ambient + Water_Added) %>% # Added treatment is sum of ambient plus water added
+  filter(Added != 0) %>% # Remove dates with no precip (from closures)
+  mutate_at(vars(Reduced, Ambient, Added), ~round(., 3)) %>% #Standardize with three decimal places
+  arrange(Day) %>%
+  mutate(Units = "mm") %>% #Recreate Units column
+  select(Day, Reduced, Ambient, Added, Units, Source1, Source2)
+
+###############################################################################
+## Save the two files that you created above
+###############################################################################
+
+## Run the one line of code below to save created FullPrecipLoma file
+## This object has Precipitation, Drought and Watering
+write.table(FullPrecipLoma,"OfficialRecords/FullPrecipLoma.csv",quote=F,row.names=F,sep=",",na="") # save daily table
+
+## Run the one line of code below to save created daily precipitation file
+## Only daily precipitation values. 
+write.table(DailyPrecip,"OfficialRecords/DailyAmbientPrecipLoma.csv",quote=F,row.names=F,sep=",",na="") # save daily table
+
---
+++ ProcessGaugeData.R
@@ -0,0 +1,81 @@
+################################################################################
+####### Script used to clean raw data that were downloaded from the   ##########
+####### OC http://hydstra.ocpublicworks.com/web.htm 			            ##########
+####### This code cleans and calculates daily precipitation 	        ##########
+####### You may need to adjust names accordingly. 					          ##########
+####### You will use the files in the folder GaugeData that are   	  ##########
+####### in the Microbes and Global Change drive 					            ##########
+####### Files in the folder GaugeData are not clean. Do not use 	    ##########
+####### files in this folder to get precipitation daily				        ##########
+################################################################################
+
+## Call your downloaded packages
+library(googledrive)
+library(dplyr)
+
+# Input Loma weather station data to process 
+Precip.UCI.raw <- drive_get("GaugeDataToClean/2022-09-30 EastLomaUCIRAWS.csv", shared_drive = "Microbes and Global Change") %>%
+  drive_read_string(encoding="UTF-8") %>%
+  read.csv(text=.,stringsAsFactors = F,header = T)
+
+# Raw data
+Precip.UCI <- Precip.UCI.raw %>%
+  rename(Date = Timestamp) %>%
+  mutate(Precipitation = as.numeric(Rain_mm)) %>% #Reformatting the column that has the ambient precipitation
+  select(c(Date,Precipitation)) %>%
+  mutate(Date = as.POSIXct(Date,format="%m/%d/%Y, %I:%M:%S %p",tz="America/Los_Angeles")) %>% # Formatting to dates in Pacific timezone
+  mutate(Day = as.character(Date,format="%m/%d/%Y",tz="")) %>%
+  filter(!is.na(Date)) %>% #Remove NA
+  mutate(Units = "mm")  %>%
+  mutate(Source = "EastLomaUCIRAWS") %>% #Make sure the source format matched the format for current clean files
+  arrange(Date) %>%
+  select(c(Date,Precipitation,Source,Day,Units))
+  
+# Save this file. This file will be added to the "RawData" folder 
+# Make sure the name includes "CleanRaw" otherwise it won't be identified by the R script
+write.table(Precip.UCI,"Outputs/2022-09-30 EastLomaUCIRAWSClean.csv",quote=F,row.names=F,sep=",",na="")
+
+# Input Hicks Canyon OC Public Works data
+Precip.Hicks.raw <- drive_get("GaugeDataToClean/HICKS_CYN_2122.csv", shared_drive = "Microbes and Global Change") %>%
+  drive_read_string(encoding="UTF-8") %>%
+  read.csv(text=.,stringsAsFactors = F,header = T)
+
+Precip.Hicks <- Precip.Hicks.raw %>%
+  slice(-1:-3) %>% #Remove first rows. You may need to adjust
+  rename(Date = Time) %>%
+  mutate(Gauge = as.numeric(HICKS_CYN)) %>% #Reformating the column that has the ambient precipitation
+  select(c(Date,Gauge)) %>%
+  mutate(Date = as.POSIXct(Date,format="%H:%M:%S %m/%d/%Y",tz="America/Los_Angeles")) %>% # Formatting to dates in Pacific timezone
+  mutate(Day = as.character(Date,format="%m/%d/%Y",tz="")) %>%
+  mutate(Precipitation = Gauge*25.4) %>% # Convert inches to mm
+  mutate(Source = "Hicks_Canyon") %>% #Make sure the source format matched the format for current clean files
+  select(c(Date,Precipitation,Source,Day)) %>%
+  filter(!is.na(Date)) %>% #Remove NA
+  mutate(Units = "mm")
+
+# Save this file. This file will be added to the "RawData" folder 
+# Make sure the name includes "CleanRaw" otherwise it won't be identified by the R script
+write.table(Precip.Hicks,"Outputs/Hicks2122.csv",quote=F,row.names=F,sep=",",na="")
+
+# Legacy code for converting gauge readings to Pacific time zone and day to a standard date format
+# Already applied to gauge data from 2013-14
+Precip2 <- read.csv("INPUT_FILE.csv",stringsAsFactors = F) %>%
+  mutate(Date = as.POSIXct(Date,format="%Y-%m-%d %H:%M:%S",tz="GMT")) %>% # Dates were recorded in UTC
+  mutate(Day = format(Date,format="%m/%d/%Y",tz="America/Los_Angeles")) %>%
+  mutate(Date = format(Date,format="%Y-%m-%d %H:%M:%S",tz="America/Los_Angeles")) # Converting to dates in Pacific timezone
+
+# Make sure the name includes "CleanRaw" otherwise it won't be identified by the R script
+write.table(Precip2,"OUTPUT_FILE.csv",quote=F,row.names=F,sep=",",na="")
+
+
+##Calculate Daily sum
+DailyPrecipLoma <- Precip %>% #assign a new name to our clean raw data
+  filter(Precipitation>0) %>% # Only keep non-zero measurements
+  group_by(Day) %>% # Group by day for easy calculation and visualization 
+  summarize(rawdata = sum(Precipitation)) %>% # Calculate daily sums
+  rename(AmbientPrecip = rawdata) %>%
+  mutate(Units = "mm") %>%
+  mutate(Source = "Hicks_Canyon")
+
+write.table(DailyPrecipLoma,"FILENAME.csv",quote=F,row.names=F,sep=",",na="") # save daily table
+
