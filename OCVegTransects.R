b358ffb966c14857a7a009b13ec8a8d9922ae2e6
Steven Allison <allisons@dhcp-v031-219.mobile.uci.edu>
2023-01-23 15:14:51 -0800

    Update transect script

--- OCVegTransects.R
+++ OCVegTransects.R
@@ -30,34 +30,22 @@ data.percent.cover.sprich <- data.percent.cover %>%
 data.percent.cover <- data.percent.cover %>% 
   full_join(data.percent.cover.sprich)
 names(data.percent.cover)
+
+# function to compute standard error
+std.error <- function(x,na.rm=T) sd(x,na.rm)/sqrt(sum(!is.na(x)))
+
 #calculate means and SE for metrics
-data.sum <- data.percent.cover %>% group_by (Year, Veg_Comm) %>% 
-  summarise(meanNative = mean(Native), 
-            seNative = sd(Native)/sqrt(length(Native)), 
-            meanNativeGrass = mean(NativeGrass), 
-            seNativeGrass = sd(NativeGrass)/sqrt(length(NativeGrass)), 
-            meanNativeShrub = mean(NativeShrub), 
-            seNativeShrub = sd(NativeShrub)/sqrt(length(NativeShrub)), 
-            meanLowNNGrass = 1-(mean(NonNativeGrass)), 
-            seNonNativeGrass = sd(NonNativeGrass)/sqrt(length(NonNativeGrass)), 
-            meanLowNNForb = 1-(mean(NonNativeForb)), 
-            seNonNativeForb = sd(NonNativeForb)/sqrt(length(NonNativeForb)),
-            meanNativeSpRich = mean(NatSpRich), 
-            seNativeSpRich = sd(NatSpRich)/sqrt(length(NatSpRich)),
-            meanMinFireReturn = mean(Min.Fire.Return.Interval.Prior.to.Sample.Year), 
-            seMinFireReturn = sd(Min.Fire.Return.Interval.Prior.to.Sample.Year)/sqrt(length(Min.Fire.Return.Interval.Prior.to.Sample.Year)),
-            meanFireFrequency = mean(Fire.Frequency._Total.Fires.since.1914.Prior.to.Sample.Year), 
-            seFireFrequency = sd(Fire.Frequency._Total.Fires.since.1914.Prior.to.Sample.Year)/sqrt(length(Fire.Frequency._Total.Fires.since.1914.Prior.to.Sample.Year)),
-            meanADEFAS = mean(ADEFAS), 
-            seADEFAS = sd(ADEFAS)/sqrt(length(ADEFAS)),
-            meanCrust = mean(C), 
-            seCrust = sd(C)/sqrt(length(C)),
-            meanBare = mean(B), 
-            seBare = sd(B)/sqrt(length(B)),
-  ) %>% ungroup()
+data.sum <- data.percent.cover %>%
+  group_by (Year, Veg_Comm) %>% 
+  summarize(across(c(Native,NativeGrass,NativeShrub,NonNativeGrass,NonNativeForb,NatSpRich,
+                     Min.Fire.Return.Interval.Prior.to.Sample.Year,
+                     Fire.Frequency._Total.Fires.since.1914.Prior.to.Sample.Year,
+                     ADEFAS,C,B),list(mean = mean, se = std.error))) %>%
+  mutate(LowNNGrass_mean = 1-NonNativeGrass_mean) %>%
+  mutate(LowNNForb_mean = 1-NonNativeForb_mean) %>%
+  ungroup()
 
 #subsetting css and gl for ltreb proposal
-
 ltreb.sum <- data.sum %>% filter(Veg_Comm != "CHAP")
 
 # LTREB Plots
@@ -65,12 +53,12 @@ ltreb.sum <- data.sum %>% filter(Veg_Comm != "CHAP")
 
 ltreb.sum %>% 
   ggplot(aes(x = Year, 
-             y = meanNative), group=Veg_Comm) +
+             y = Native_mean), group=Veg_Comm) +
   geom_line(linetype="dashed", color='#00BA38') +
   geom_point(size=2, color='#00BA38') +
   facet_grid(.~Veg_Comm) +
-  geom_errorbar(aes(ymin = meanNative-seNative, 
-                    ymax = meanNative+seNative),
+  geom_errorbar(aes(ymin = Native_mean-Native_se, 
+                    ymax = Native_mean+Native_se),
                 width = .1, color='#00BA38') +
   labs(x = "Year",
        y = "Combined Cover of All Native Veg") +
