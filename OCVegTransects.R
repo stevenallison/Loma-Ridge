f5b308affc2e7a8c49fa2a1d386353f19919fbab
Steven Allison <allisons@dhcp-v031-219.mobile.uci.edu>
2023-03-23 13:35:51 -0700

    Update transect graphics

--- OCVegTransects.R
+++ OCVegTransects.R
@@ -46,18 +46,28 @@ data.sum <- data.percent.cover %>%
   ungroup()
 
 #subsetting css and gl for ltreb proposal
-ltreb.sum <- data.sum %>% filter(Veg_Comm != "CHAP", Year != 2007)
+ltreb.sum <- data.sum %>% filter(Veg_Comm != "CHAP", Year != 2007) %>%
+  mutate(Veg_Comm = str_replace(Veg_Comm,"GL","Grassland"))
 
 # LTREB Plots
-#replace meanNative and seNative with variables from ltreb.sum
-
-ggplot(ltreb.sum, aes(x = Year, y = Native_mean, group=Veg_Comm, color=Veg_Comm, linetype=Veg_Comm, shape=Veg_Comm)) +
-geom_errorbar(aes(ymin = Native_mean-Native_se, 
-                  ymax = Native_mean+Native_se),
-              width = .1) +
-geom_line() +
-geom_point() +
-labs(x = "Year",
-     y = "Combined Cover of All Native Veg") +
-scale_color_manual(values=c('#619CFF','#00BA38')) +
-theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+png("Graphics/Transects.png",width = 8,height = 4,units = "in",res=300)
+ggplot(ltreb.sum, aes(x = Year, y = Native_mean, group=Veg_Comm, color=Veg_Comm, linetype=Veg_Comm, shape=Veg_Comm)) + 
+  geom_errorbar(aes(ymin = Native_mean-Native_se, ymax = Native_mean+Native_se),width = .1) +
+  geom_line() +
+  geom_point(size = 2) +
+  labs(x = "Year",
+       y = "Native cover (%)") +
+  scale_color_manual(values=c('coral1','black')) +
+  theme_bw(base_size=16) +
+  theme(plot.title = element_text(hjust=0, size=18),
+        axis.text.y=element_text(size=14),
+        axis.text.x=element_text(size=14),
+        axis.title.y=element_text(size=18),
+        axis.title.x=element_text(size=18),
+        legend.position=c(0.3,0.85), 
+        legend.title = element_blank(),
+        legend.key.width= unit(1.5, 'cm'),
+        legend.text = element_text(size=12),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+dev.off()
